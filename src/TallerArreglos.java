80035e5cc260b2d515f721787807b44924245d1e
SENA <mmm808383@gmail.com>
2024-08-05 07:38:47 -0500

    Finalizacion primera parte

--- src/TallerArreglos.java
+++ src/TallerArreglos.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class TallerArreglos {
     public static void main(String[] args) {
         // 1. Declaración e inicialización de un arreglo
@@ -8,6 +10,7 @@ public class TallerArreglos {
         imprimirArreglo(numeros);
 
         // 3. Encontrar el número mayor
+
         int mayor = encontrarMayor(numeros);
         System.out.println("El número mayor es: " + mayor);
 
@@ -17,7 +20,7 @@ public class TallerArreglos {
 
         // 5. Invertir el arreglo
         invertirArreglo(numeros);
-        System.out.println("Arreglo invertido:");
+        System.out.println("Arreglo invertido: ");
         imprimirArreglo(numeros);
 
         // 6. Ordenar el arreglo (implementa el método ordenarArreglo)
@@ -26,33 +29,52 @@ public class TallerArreglos {
         imprimirArreglo(numeros);
     }
 
+    //METODOS DE FUNCIONALIDAD
+
     // Método para imprimir el arreglo
-    public static void imprimirArreglo(int[] arr) {
-        for (int num : arr) {
+    public static void imprimirArreglo(int[] numeros ) {
+        for (int num : numeros) {
             System.out.print(num + " ");
         }
         System.out.println();
     }
 
     // Método para encontrar el número mayor
-    public static int encontrarMayor(int[] arr) {
-        // Implementa este método
-        return 0;
+    public static int encontrarMayor(int[] numeros) {
+     int numeroMayor = numeros[0];
+     for(int i = 1; i < numeros.length; i++){
+         if (numeros[i] > numeroMayor){
+             numeroMayor = numeros[i];
+         }
+     }
+        return numeroMayor;
     }
 
     // Método para calcular el promedio
-    public static double calcularPromedio(int[] arr) {
-        // Implementa este método
-        return 0.0;
+    public static double calcularPromedio(int[] numeros) {
+        double total = 0;
+        for (int i = 0; i < numeros.length; i++){
+            total += numeros[i];
+        }
+        double totalFinal = total / numeros.length;
+        return totalFinal;
     }
 
+
     // Método para invertir el arreglo
-    public static void invertirArreglo(int[] arr) {
-        // Implementa este método
+    public static void invertirArreglo(int[] numeros) {
+        int invertido[] = new int[numeros.length];
+        for (int i = invertido.length - 1, j = 0; i >= 0; i--, j++){
+            invertido[i] = numeros[j];
+
+        }
+        for (int i = 0; i < invertido.length; i ++){
+            System.out.print(invertido[i] +" ");
+        }
     }
 
     // Método para ordenar el arreglo (puedes usar el algoritmo de burbuja)
-    public static void ordenarArreglo(int[] arr) {
-        // Implementa este método
+    public static void ordenarArreglo(int[] numeros) {
+        Arrays.sort(numeros);;
     }
 }
